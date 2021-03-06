2c236173dabd5dd021975975118e917168102151
paytonealexis <paytonealexis@gmail.com>
2021-03-06 08:19:52 -0800

    Update s3.tf
    Adding some new s3 buckets

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,8 +1,4 @@
 resource "aws_s3_bucket" "data" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
@@ -23,9 +19,6 @@ resource "aws_s3_bucket_object" "data_object" {
 }
 
 resource "aws_s3_bucket" "financials" {
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
   force_destroy = true
@@ -37,8 +30,6 @@ resource "aws_s3_bucket" "financials" {
 }
 
 resource "aws_s3_bucket" "operations" {
-  # bucket is not encrypted
-  # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
   acl    = "private"
   versioning {
@@ -53,7 +44,6 @@ resource "aws_s3_bucket" "operations" {
 }
 
 resource "aws_s3_bucket" "data_science" {
-  # bucket is not encrypted
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
